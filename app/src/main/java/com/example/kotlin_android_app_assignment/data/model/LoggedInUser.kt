345de47f44d38c4ce6a33b53cf3ca035374a409a
Chung2212 <22219056@life.hkbu.edu.hk>
2023-03-27 16:28:06 +0800

    init 1

---
+++ app/src/main/java/com/example/kotlin_android_app_assignment/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin_android_app_assignment.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
